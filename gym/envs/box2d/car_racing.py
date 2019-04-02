cda63294c95019800ba1698aee097d91b661a37b
NotAnyMike <mikewoodcockc@gmail.com>
2019-04-02 10:17:52 +0100

    fixing #12

--- gym/envs/box2d/car_racing.py
+++ gym/envs/box2d/car_racing.py
@@ -250,8 +250,11 @@ class CarRacing(gym.Env, EzPickle):
             allow_reverse=0,
             min_episode_reward=-np.inf,
             max_episode_reward=+np.inf,
+            animate_zoom=False
             ):
-
+        
+        # Animate zoom
+        self.animate_zoom = animate_zoom
         
         # Number of lanes, 1 or 2
         self.num_lanes = num_lanes  if num_lanes in [1,2] else 1
@@ -1137,7 +1140,10 @@ class CarRacing(gym.Env, EzPickle):
 
         if "t" not in self.__dict__: return  # reset() not called yet
 
-        zoom = 0.1*SCALE*max(1-self.t, 0) + ZOOM*SCALE*min(self.t, 1)   # Animate zoom first second
+        if self.animate_zoom:
+            zoom = 0.1*SCALE*max(1-self.t, 0) + ZOOM*SCALE*min(self.t, 1)   # Animate zoom first second
+        else:
+            zoom = ZOOM*SCALE
         zoom_state  = ZOOM*SCALE*STATE_W/WINDOW_W
         zoom_video  = ZOOM*SCALE*VIDEO_W/WINDOW_W
         scroll_x = self.car.hull.position[0]
